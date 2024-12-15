07057f55cb76e4529ba970ec8ea0f019b2aa7183
almirari <almirari004@gmail.com>
2024-12-15 20:39:31 +0700

    register testing successful

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,13 @@
+import Container from "../components/Container";
+import FormWrap from "../components/FormWrap";
+import LoginForm from "./LoginForm";
+
 const Login = () => {
-    return (  );
+    return ( <Container>
+        <FormWrap>
+            <LoginForm/>
+        </FormWrap>
+    </Container> );
 }
  
 export default Login;
--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,6 +7,10 @@ import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 import Button from "../components/Button";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
+import axios from "axios";
+import toast from "react-hot-toast";
+import {signIn} from 'next-auth/react';
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
     const [isLoading, setIsLoading] = useState(false)
@@ -17,10 +21,30 @@ const RegisterForm = () => {
             password: "",
         },
     });
+
+    const router = useRouter()
  
     const onSubmit:SubmitHandler<FieldValues> = (data) => {
-        setIsLoading(true)
-        console.log(data)
+        setIsLoading(true);
+        axios.post('/api/register', data).then(()=> {
+            toast.success('Account created')
+            signIn('credentials',{
+                email: data.email,
+                password: data.password,
+                redirect: false,
+            }).then((callback) => {
+                if(callback?.ok){
+                    router.push('/cart');
+                    router.refresh();toast.success('logged in');
+                }
+
+                if(callback?.error){
+                    toast.error(callback.error);
+                }
+            });
+        }).catch(() => toast.error("Something went wrong.")).finally(() => {
+    setIsLoading(false)
+    });
     };
 
     return ( <>
